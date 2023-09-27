0ab41dacecf9ff4d2cffea7b3172303c7b289a00
Emese Padanyi <emese.padanyi@genesys.com>
2023-09-27 15:06:50 +0200

    Unify size conversions

--- src/converters/paragraph.ts
+++ src/converters/paragraph.ts
@@ -1,20 +1,18 @@
 import { DomNode } from 'html-parse-stringify';
-import { StyleAttribute } from '../models/html';
+import { getLength } from '../utils/length';
 import {
   DocumentBodyBlockAlignType,
-  DocumentBodyBlockFontType,
+  DocumentBodyBlockFontType
 } from '../models/blocks/document-body-block';
+import {
+  DocumentBodyParagraph, DocumentBodyParagraphBlock, DocumentBodyParagraphProperties
+} from '../models/blocks/document-body-paragraph';
+import { StyleAttribute } from '../models/html';
 import {
   generateEmptyTextBlock,
   generateTextBlocks,
-  postProcessTextBlocks,
+  postProcessTextBlocks
 } from './text';
-import {
-  DocumentBodyParagraph,
-  DocumentBodyParagraphProperties,
-  DocumentBodyParagraphBlock,
-} from '../models/blocks/document-body-paragraph';
-import { truncateToSinglePrecisionFloat } from '../utils';
 
 export const generateParagraphBlock = (
   domElement: DomNode,
@@ -66,10 +64,7 @@ const generateProperties = (
       .map((keyValue) => {
         if (keyValue.length === 2) {
           if (keyValue[0] === StyleAttribute.PaddingLeft) {
-            // remove the em from the value
-            indentation = truncateToSinglePrecisionFloat(
-              Number(keyValue[1].replace(/\s*em\s*/g, '')),
-            );
+            indentation = getLength(keyValue[1]);
           }
           if (keyValue[0] === StyleAttribute.TextAlign) {
             align = cssTextAlignToAlignType(keyValue[1]);
